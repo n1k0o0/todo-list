3b2b8e4c4bd44a59414036f9f5bbd689ebd03a03
Nicat Ismayilov <n1ko.net.dev@gmail.com>
2024-01-23 17:47:46 +0400

    fix: migration, api

--- database/migrations/2024_01_23_064140_create_todos_table.php
+++ database/migrations/2024_01_23_064140_create_todos_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('todos',static function (Blueprint $table) {
+        Schema::create('todos', static function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_done')->default('false');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
